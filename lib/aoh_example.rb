8629baa65eaba8748e409fa72db983688124864d
Zach Bayard <zbayard@gmail.com>
2020-10-28 15:52:33 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -13,6 +13,13 @@ def assembled_aoh
 end
 
 def literal_aoh
+  [
+    { name:  "Don Gately", occupation: "Live-in Staff" },
+    { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+    { name:  "Pat Monteseian", occupation: "Staff" },
+    { name:  "Kate Gompert", occupation: "None" },
+    { name:  "Bruce Green", occupation: "Fan of Mildred" }
+    ]
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 end
